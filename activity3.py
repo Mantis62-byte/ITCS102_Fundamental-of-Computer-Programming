c17b2de65ff0baf3e5599c270f00dfbe5b12b747
John Paul Mantis <jpaulmoulic13@gmail.com>
2026-08-24 10:59:42 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,20 @@
+#using variables
+
+#int
+age = 22
+class_size = 49
+
+#float
+temp = 34.2
+speed = 30.4
+
+#string
+username = "john paul"
+password = "mahal ko"
+last_attended_school = "Gulang Gulang National School"
+
+
+#boolen
+isRich = False
+
+print("my age is" , age)
