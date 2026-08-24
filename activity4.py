d97b0b749e6aa98ad31b01fa8b1a8e88dfb3f723
John Paul Mantis <jpaulmoulic13@gmail.com>
2026-08-24 11:27:47 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,11 @@
+#escape sequence -> to modify the behavior of linear formatted string
+# \n- new line
+# \t- tab space
+# \r- carriage return \ deprecated ( obsolette
+# \b- back space
+# \\- insert slash
+# \"- insert double quotation
+
+phrase1 = "to fast to live, \n\nnto fast to die, \n\n\naray ko po"
+
+print(phrase1)
