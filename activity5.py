67f1047babc8cb281c366fa4a5e1e08c42391e81
John Paul Mantis <jpaulmoulic13@gmail.com>
2026-08-26 11:20:53 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,13 @@
+#operators activity
+
+a = 7
+b = 5 
+sum1 = a + b
+difference1 = a - b
+product1 = a * b 
+qoutient1 = a / b
+
+print("The sum of ",a," and", b," is",sum1)
+print(" the difference of", a, "and", b, "is",difference1)
+print("the product of" ,a * b," =",product1)
+print("the quotient of", a / b," =",qoutient1)
