0245d8f12705cac9d735cd2d66e5fb14b6df1cc5
John Paul Mantis <jpaulmoulic13@gmail.com>
2026-09-07 10:48:01 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,23 @@
+#relational operator activity
+
+a = 5
+b = 5
+
+password1 = "sample"
+password2 = "sample"
+
+#1
+print ( a > b )
+#2
+print ( a < b ) 
+#3
+print ( a > b )
+#4
+print ( a >= b ) 
+#5
+print ( a <= b )
+#6
+print ( a == b ) 
+print(password1 == password2)
+#6 !=
+print(password1 !=  password2)
