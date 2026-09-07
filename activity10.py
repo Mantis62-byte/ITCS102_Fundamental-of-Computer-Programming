4f4135e58fc5c7d886ac598fa59f57f511ae27d2
John Paul Mantis <jpaulmoulic13@gmail.com>
2026-09-07 11:15:05 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,14 @@
+#basic if else program
+
+username = "JP"
+password = "MANTIS"
+
+u = input("input USERNAME ---> ")
+p = input("input PASSWORD ---> ")
+
+if u == username or  p  == password:
+        print("username  and password correct")
+
+else:
+        print(" access denied")
+
